d970a13f90c979f2acf588e6ffa9d95a70e9c3ed
TerryThreatt <terry.threatt@gmail.com>
2020-08-26 14:18:05 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(record) {
+  let win = record.find( record => record.result === "W" )
+  if (win) {
+    return win.year
+  } else {
+    return undefined
+  }
+}
